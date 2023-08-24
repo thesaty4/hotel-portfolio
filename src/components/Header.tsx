55502da08190526a06673f82c190631058ed7749
Satya Mishra <the.satya559@gmail.com>
2023-08-24 23:18:10 +0530

    fix: logo and header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import React from "react";
 
 interface HeaderProps {
   title: string;
@@ -9,40 +8,88 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = ({ title, onLogin }) => {
   return (
     <header className="top-navbar">
-		<nav className="navbar navbar-expand-lg navbar-light bg-light">
-			<div className="container">
-				<a className="navbar-brand" href="index.html">
-					<img src="images/logo.png" alt="" />
-				</a>
-				<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbars-rs-food" aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
-				  <span className="navbar-toggler-icon"></span>
-				</button>
-				<div className="collapse navbar-collapse" id="navbars-rs-food">
-					<ul className="navbar-nav ml-auto">
-						<li className="nav-item active"><a className="nav-link" href="index.html">Home</a></li>
-						<li className="nav-item"><a className="nav-link" href="menu.html">Menu</a></li>
-						<li className="nav-item"><a className="nav-link" href="about.html">About</a></li>
-						<li className="nav-item dropdown">
-							<a className="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown">Pages</a>
-							<div className="dropdown-menu" aria-labelledby="dropdown-a">
-								<a className="dropdown-item" href="reservation.html">Reservation</a>
-								<a className="dropdown-item" href="stuff.html">Stuff</a>
-								<a className="dropdown-item" href="gallery.html">Gallery</a>
-							</div>
-						</li>
-						<li className="nav-item dropdown">
-							<a className="nav-link dropdown-toggle" href="#" id="dropdown-a" data-toggle="dropdown">Blog</a>
-							<div className="dropdown-menu" aria-labelledby="dropdown-a">
-								<a className="dropdown-item" href="blog.html">blog</a>
-								<a className="dropdown-item" href="blog-details.html">blog Single</a>
-							</div>
-						</li>
-						<li className="nav-item"><a className="nav-link" href="contact.html">Contact</a></li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-	</header>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          <a className="navbar-brand" href="index.html">
+            <img src={require("../assets/images/logo.png")} alt="" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbars-rs-food"
+            aria-controls="navbars-rs-food"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbars-rs-food">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="index.html">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="menu.html">
+                  Menu
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="about.html">
+                  About
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="dropdown-a"
+                  data-toggle="dropdown"
+                >
+                  Pages
+                </a>
+                <div className="dropdown-menu" aria-labelledby="dropdown-a">
+                  <a className="dropdown-item" href="reservation.html">
+                    Reservation
+                  </a>
+                  <a className="dropdown-item" href="stuff.html">
+                    Stuff
+                  </a>
+                  <a className="dropdown-item" href="gallery.html">
+                    Gallery
+                  </a>
+                </div>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="dropdown-a"
+                  data-toggle="dropdown"
+                >
+                  Blog
+                </a>
+                <div className="dropdown-menu" aria-labelledby="dropdown-a">
+                  <a className="dropdown-item" href="blog.html">
+                    blog
+                  </a>
+                  <a className="dropdown-item" href="blog-details.html">
+                    blog Single
+                  </a>
+                </div>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="contact.html">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
   );
 };
 
