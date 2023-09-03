3f1b1db6fa4e4b1282259adc139066008ecc6f19
Satya Mishra <the.satya559@gmail.com>
2023-09-03 12:41:10 +0530

    backend integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
-import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import hotel from './constant/hotel-details'
-import Main from './components/Main';
-import './assets/css/style.css'
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import hotel from "./constant/hotel-details";
+import Main from "./components/Main";
+import "./assets/css/style.css";
 const App: React.FC = () => {
   const handleLogin = () => {
     // Implement your login logic here
-    alert('Login clicked!');
+    alert("Login clicked!");
   };
 
+  const [data, setData] = React.useState(null);
+  React.useEffect(() => {
+    fetch("http://localhost:5000")
+      .then((res) => res.json())
+      .then((res) => setData(res.message));
+  }, []);
+  console.log("*********", data);
   return (
-    <div className='main-wrapper'>
+    <div className="main-wrapper">
       <Header title={hotel.name.toUpperCase()} onLogin={handleLogin} />
-      <Main/>
-      <Footer socialLinks={hotel.socialLinks} hotelName={hotel.name} year={hotel.copyright.year}/>
+      <Main />
+      <Footer
+        socialLinks={hotel.socialLinks}
+        hotelName={hotel.name}
+        year={hotel.copyright.year}
+      />
     </div>
   );
 };
