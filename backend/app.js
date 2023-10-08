cf6e75b199bb7db19336713e79709e0125c6a634
Satya Mishra <the.satya559@gmail.com>
2023-10-08 17:58:33 +0530

    fix data not capturing at the server side

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser');
 
 const cors = require('cors');
 
@@ -11,6 +12,7 @@ var fetchRouter = require('./routes/fetch-route');
 
 // Interceptor 
 app.use(cors());
+app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
