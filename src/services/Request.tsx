d80921adedd67c0e2eabacfd0c9689ea0922b1bf
Satya Mishra <the.satya559@gmail.com>
2023-10-08 17:59:57 +0530

    api versioning...

--- src/services/Request.tsx
+++ src/services/Request.tsx
@@ -2,6 +2,6 @@ import RequestService from "../shared/service/RequestService";
 
 export default class Request extends RequestService {
   getSiteInfo<SiteInfo>(): Promise<SiteInfo> {
-    return this.get(`/site-info`);
+    return this.get(`/api/v1/site-info`);
   }
 }
--- src/shared/service/RequestService.tsx
+++ src/shared/service/RequestService.tsx
@@ -3,19 +3,30 @@ export default class RequestService {
   private baseURL = "http://localhost:5000";
 
   private async makeRequest<T>(
-    method: "get" | "post" | "put" | "patch",
+    method: "get" | "post" | "put" | "patch" | "delete",
     endpoint: string,
-    data?: any
+    data?: T
   ): Promise<T> {
     try {
       const response: AxiosResponse<T> = await axios({
         method,
         url: `${this.baseURL}${endpoint}`,
-        data,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: JSON.stringify(data),
       });
       return response.data;
     } catch (error: any) {
-      throw new Error(`Request failed: ${error.message}`);
+      if (error.response && error.response.data && error.response.data.error) {
+        // If there's an error message from the server, return it
+        throw new Error(error.response.data.error);
+      } else {
+        // If no specific error message from the server, throw a generic error
+        const errorMessage = `Request failed: ${error.message}`;
+        console.error(errorMessage);
+        throw new Error(errorMessage);
+      }
     }
   }
 
@@ -23,8 +34,12 @@ export default class RequestService {
     return this.makeRequest("get", endpoint);
   }
 
-  async post<T>(endpoint: string, data?: any): Promise<T> {
+  async post<T>(endpoint: string, data?: T): Promise<T> {
     return this.makeRequest("post", endpoint, data);
+  }
+
+  async delete<T>(endpoint: string, data?: T): Promise<T> {
+    return this.makeRequest("delete", endpoint, data);
   }
 
   async put<T>(endpoint: string, data?: any): Promise<T> {
